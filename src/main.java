92a5eda90911005fb7d344dc09068676c7557845
mike449 <39586696+mike449@users.noreply.github.com>
2019-05-12 21:14:15 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+
+public class main {
+
+	private static String url ="https://lamp.ii.us.edu.pl/~mtdyd/zawody/";
+	private WebDriver wd;
+	
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+	
+	
+}
