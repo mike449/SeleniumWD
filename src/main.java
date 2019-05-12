1ae3f37e0659915520cb0ec4bbd62021acf76068
mike449 <39586696+mike449@users.noreply.github.com>
2019-05-12 22:53:34 +0200

    v0.4
    Wszystko działa, ale przydałoby się całość ładniej skomponować.

--- src/main.java
+++ src/main.java
@@ -7,22 +7,26 @@ public class main {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		System.out.println("Test nr 1: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 2: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 3: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 4: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 5: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 6: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 7: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 8: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 9: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 10: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 11: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 12: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 13: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 14: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 15: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
-		System.out.println("Test nr 16: " + test("Jan", "Kowalski", "12-12-1999", true, true, "Dorosly"));
+		
+		String wyniki = "";
+		wyniki+=("Test nr 1: " + test("Jan", "Kowalski", "10-04-2010", false, false, "Brak kwalifikacji"))+"\n";
+		wyniki+=("Test nr 2: " + test("Jan", "Kowalski", "10-02-2009", false, true, "Brak kwalifikacji"))+"\n";
+		wyniki+=("Test nr 3: " + test("Jan", "Kowalski", "05-02-2007", true, false, "Brak kwalifikacji"))+"\n";
+		wyniki+=("Test nr 4: " + test("Jan", "Kowalski", "21-03-1953", false, false, "Brak kwalifikacji"))+"\n";
+		wyniki+=("Test nr 5: " + test("Jan", "Kowalski", "12-03-2008", true, true, "Skrzat"))+"\n";
+		wyniki+=("Test nr 6: " + test("Jan", "Kowalski", "21-01-2004", true, true, "Junior"))+"\n";
+		wyniki+=("Test nr 7: " + test("Jan", "Kowalski", "11-02-2001", false, false, "Dorosly"))+"\n";
+		wyniki+=("Test nr 8: " + test("Jan", "Kowalski", "21-03-1953", false, true, "Senior"))+"\n";
+		wyniki+=("Test nr 9: " + test("Jan", "Kowalski", "11-03-1948", true, true, "Blad danych"))+"\n";
+		wyniki+=("Test nr 10: " + test("Jan", "Kowalski", "11-01-1998", true, false, "Blad danych"))+"\n";
+		wyniki+=("Test nr 11: " + test("Jan", "Kowalski", "11-02-1996", false, true, "Blad danych"))+"\n";
+		wyniki+=("Test nr 12: " + test("Jan", "Kowalski", "10-03-1996", true, true, "Blad danych"))+"\n";
+		wyniki+=("Test nr 13: " + test("Jan", "Kowalski", "11-02-1954", false, false, "Dorosly"))+"\n";
+		wyniki+=("Test nr 14: " + test("Jan", "Kowalski", "13-03-2009", true, true, "Skrzat"))+"\n";
+		wyniki+=("Test nr 15: " + test("Jan", "Kowalski", "11-02-2002", true, true, "Junior"))+"\n";
+		wyniki+=("Test nr 16: " + test("Jan", "Kowalski", "13-02-1803", false, true, "Blad danych"));
+		
+		System.out.println(wyniki);
 	}
 
 	public static String test(String im, String nazw, String wiek, boolean zgoda, boolean badania,
@@ -54,11 +58,12 @@ public class main {
 		Thread.sleep(100);
 		wd.switchTo().alert().accept();
 		WebElement wynik = wd.findElement(By.id("returnSt"));
+		//System.out.println(wynik.getText());
 		if (wynik.getText().contains(oczekiwanyWynik)) {
 			wd.close();
 			return "ok";
 		}
-
+		wd.close();
 		return "blad";
 	}
 
